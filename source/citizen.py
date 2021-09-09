df81ece84f6b7a269b23ef213160bdbb869d4d7c
mank34 <87854830+mank34@users.noreply.github.com>
2021-09-09 15:23:29 +0200

    Update citizen.py

--- source/citizen.py
+++ source/citizen.py
@@ -1,1 +1,14 @@
+from envVar import *
 
+
+# Class for the tile entity
+class citizen(pygame.sprite.Sprite):
+
+    def __init__(self, num, img, w, h, parent=0, name="null"):
+        super().__init__()
+        
+        # Human need
+        self.hungry = 0
+        self.tired  = 0
+        
+        self.happy = 100 - self.hungry - self.tired
